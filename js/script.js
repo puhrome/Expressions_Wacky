fc67e65d4bf41903a956277ea35df145a4ee23ed
puhrome <phirom.yim@fullsail.edu>
2014-04-13 19:05:10 -0400

    Expressions_Wacky
    Using WebStorm to concatenate string variables to write a Haiku about
    my Cat.

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/**
+ * Haiku about my Cat Created by Phirom Yim on 4/13/14.
+ */
+//Haiku formula is 5 - 7 - 5
+
+stringVar= "5"; //casting number 5 as a string
+stringVar2="7";//casting number 7 as a string
+var form = stringVar + " " + stringVar2 + " " + stringVar;
+console.log(form + " is the Haiku formula");//concatenating string
+
+var firstLine = "My cat likes to meow"; //declaring first variable line
+var secondLine = "Sleep in late, he will wake you";//declaring second variable line
+var thirdLine = "With many kisses and cuddles"; //declaring third variable line
+var result = firstLine + "\n" + secondLine + "\n" + thirdLine;
+console.log(result);//print concatenating result
